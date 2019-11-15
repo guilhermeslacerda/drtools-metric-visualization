6ec10519fd97baa6f4598d154c28da12ca515c1b
Guilherme Lacerda <guilhermeslacerda@gmail.com>
2019-11-14 22:12:19 -0300

    update version, including new visualizations like cyclic dependencies and type coupling

--- drtools-properties.js
+++ drtools-properties.js
@@ -7,11 +7,12 @@
     "datasetNamespaces" : "./datasets/DrToolsMetric/drtools-metric-namespaces.csv",
     "datasetTypes" : "./datasets/DrToolsMetric/drtools-metric-types.csv",
     "datasetMethods" : "./datasets/DrToolsMetric/drtools-metric-methods.csv",
+    "datasetInternalTypeDependencies" : "./datasets/DrToolsMetric/drtools-metric-internal-dependencies.json",
     "datasetInternalDependencies" : "./datasets/DrToolsMetric/drtools-metric-internal-dependencies.json"
 };*/
 
 // Project: Software Pathfinder
-var optionsProject = {
+/*var optionsProject = {
     "projectName" : "Software Pathfinder",
     "datasetDir": "./datasets/Pathfinder/",
     "datasetSummary" : "./datasets/Pathfinder/drtools-metric-summary.csv",
@@ -21,7 +22,21 @@ var optionsProject = {
     //"datasetTypes" : "./datasets/Pathfinder/drtools-metric-types.csv",
     "datasetMethods" : "./datasets/Pathfinder/drtools-metric-methodsTop30.csv",
     //"datasetMethods" : "./datasets/Pathfinder/drtools-metric-methods.csv",
-    "datasetInternalDependencies" : "./datasets/Pathfinder/drtools-metric-internal-dependencies.json"
+    "datasetInternalDependencies" : "./datasets/Pathfinder/drtools-metric-internal-dependencies.json",
+};*/
+
+// Project: Findbugs
+var optionsProject = {
+    "projectName" : "Findbugs 3.0.1",
+    "datasetDir": "./datasets/FindBugs/",
+    "datasetSummary" : "./datasets/FindBugs/drtools-metric-summary.csv",
+    "datasetNamespaceCoupling" : "./datasets/FindBugs/drtools-metric-coupling.csv",
+    "datasetNamespaces" : "./datasets/FindBugs/drtools-metric-namespaces.csv",
+    "datasetTypes" : "./datasets/Pathfinder/drtools-metric-types.csv",
+    "datasetMethods" : "./datasets/Pathfinder/drtools-metric-methods.csv",
+    "datasetInternalTypeDependencies" : "", 
+    "datasetInternalDependencies" : "./datasets/FindBugs/drtools-metric-internal-dependenciesTop40.json",
+    "datasetCyclicDependencies" : "./datasets/FindBugs/drtools-metric-cyclic-dependencies.csv"
 };
 
 // Project: AcmeLibrary
@@ -58,11 +73,32 @@ var graphTitles = {
     "titleInternalDependencies" : "Internal Dependencies Visualization",
     "titleInternalDependenciesExplanation" : "Internal dependencies between type/classes",
 
-    "titleCoupling" : "Coupling (Input and Output) Visualization",
-    "titleCouplingExplanation" : "Red lines (output coupling) and green lines (input coupling)",
+    "titleTypeCoupling1" : "Type Coupling Visualization",
+    "titleTypeCoupling1Explanation" : "Red lines (output coupling) and green lines (input coupling)",
+
+    "titleTypeCoupling2" : "Type Coupling Visualization",
+    "titleTypeCoupling2Explanation" : "Types with I-DEP (y axis), DEP (x axis), FAN-IN (bubble color), and FAN-OUT (bubble size)",
 
     "titleThresholds" : "Metric Thresholds Information",
 
     "titleInstability" : "Instability and Abstractness Visualization",
-    "titleInstabilityExplanation" : "Abstractness degree (y axis) and Instability (x axis)"
+    "titleInstabilityExplanation" : "Abstractness degree (y axis) and Instability (x axis)",
+
+    "titleCyclicDependencies" : "Cyclic Dependencies Visualization",
+    "titleCyclicDependenciesExplanation" : "Cyclic dependencies between type/classes"
+};
+
+// Titles
+var errorMessage = {
+    "msgSummary" : "File not found... It is not possible to visualize data about summary",
+    "msgNamespaces" : "File not found... It is not possible to visualize data about namespaces",
+    "msgTypes" : "File not found... It is not possible to visualize data about types",
+    "msgMethods" : "File not found... It is not possible to visualize data about methods",
+    "msgInternalDependencies" : "File not found... It is not possible to visualize data about internal types dependencies",
+    "msgNamespaceCoupling" : "File not found... It is not possible to visualize data about namespace coupling",
+    "msgTypeCoupling1" : "File not found... It is not possible to visualize data about type coupling",
+    "msgIAD" : "File not found... It is not possible to visualize data about instability, abstractness, and distance",
+    "msgInstability" : "File not found... It is not possible to visualize data about instability and abstractness",
+    "msgTypeCoupling2" : "File not found... It is not possible to visualize data about type coupling",
+    "msgCyclicDependencies" : "File not found... It is not possible to visualize data about cyclic dependencies"
 };
