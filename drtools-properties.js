e747524a85f3916282d5eedcf84581fd95c842f9
Guilherme Lacerda <guilhermeslacerda@gmail.com>
2020-02-17 14:46:42 -0300

    update drtools-properties.js and add LICENSE.txt

--- drtools-properties.js
+++ drtools-properties.js
@@ -7,13 +7,13 @@
     "datasetNamespaceCoupling" : "./datasets/DrToolsMetric/drtools-metric-coupling.csv",
     "datasetNamespaces" : "./datasets/DrToolsMetric/drtools-metric-namespaces.csv",
     "datasetTypes" : "./datasets/DrToolsMetric/drtools-metric-types.csv",
-    "datasetMethods" : "./datasets/DrToolsMetric/drtools-metric-methods.csv",
+    "datasetMethods" : "./datasets/DrToolsMetric/drtools-metric-methods-top60.csv",
     "datasetInternalTypeDependencies" : "./datasets/DrToolsMetric/drtools-metric-internal-dependencies.json",
     "datasetInternalDependencies" : "./datasets/DrToolsMetric/drtools-metric-internal-dependencies.json"
 };*/
 
 // Project: Software Pathfinder
-/*var optionsProject = {
+var optionsProject = {
     "projectName" : "Software Pathfinder",
     "datasetDir": "./datasets/Pathfinder/",
     "datasetSummary" : "./datasets/Pathfinder/drtools-metric-summary.csv",
@@ -24,23 +24,39 @@
     //"datasetTypes" : "./datasets/Pathfinder/drtools-metric-types.csv",
     "datasetMethods" : "./datasets/Pathfinder/drtools-metric-methodsTop30.csv",
     //"datasetMethods" : "./datasets/Pathfinder/drtools-metric-methods.csv",
+    "datasetInternalTypeDependencies" : "./datasets/JDepend/drtools-metric-internal-dependencies.json", 
     "datasetInternalDependencies" : "./datasets/Pathfinder/drtools-metric-internal-dependencies.json",
-};*/
+};
 
 // Project: Findbugs
-var optionsProject = {
+/*var optionsProject = {
     "projectName" : "Findbugs 3.0.1",
     "datasetDir": "./datasets/FindBugs/",
     "datasetSummary" : "./datasets/FindBugs/drtools-metric-summary.csv",
     "datasetResonance" : "./datasets/FindBugs/drtools-metric-resonance.json",
     "datasetNamespaceCoupling" : "./datasets/FindBugs/drtools-metric-coupling.csv",
     "datasetNamespaces" : "./datasets/FindBugs/drtools-metric-namespaces.csv",
-    "datasetTypes" : "./datasets/Pathfinder/drtools-metric-types.csv",
-    "datasetMethods" : "./datasets/Pathfinder/drtools-metric-methods.csv",
+    "datasetTypes" : "./datasets/FindBugs/drtools-metric-types-top40.csv",
+    "datasetMethods" : "./datasets/FindBugs/drtools-metric-methods-top40.csv",
     "datasetInternalTypeDependencies" : "", 
     "datasetInternalDependencies" : "./datasets/FindBugs/drtools-metric-internal-dependenciesTop40.json",
     "datasetCyclicDependencies" : "./datasets/FindBugs/drtools-metric-cyclic-dependencies.csv"
-};
+};*/
+
+// Project: JDepend
+/*var optionsProject = {
+    "projectName" : "JDepend",
+    "datasetDir": "./datasets/JDepend/",
+    "datasetSummary" : "./datasets/JDepend/drtools-metric-summary.csv",
+    "datasetResonance" : "./datasets/JDepend/drtools-metric-resonance.json",
+    "datasetNamespaceCoupling" : "./datasets/JDepend/drtools-metric-namespace-coupling.csv",
+    "datasetNamespaces" : "./datasets/JDepend/drtools-metric-namespaces.csv",
+    "datasetTypes" : "./datasets/JDepend/drtools-metric-types.csv",
+    "datasetMethods" : "./datasets/JDepend/drtools-metric-methods.csv",
+    "datasetInternalTypeDependencies" : "./datasets/JDepend/drtools-metric-internal-dependencies.json", 
+    "datasetInternalDependencies" : "./datasets/JDepend/drtools-metric-internal-dependencies.json",
+    "datasetCyclicDependencies" : ""
+};*/
 
 // Titles
 var graphTitles = {
@@ -59,10 +75,10 @@ var graphTitles = {
     "titleNamespaceCouplingExplanation" : "CA (Afferent Coupling) and CE (Efferent Coupling)",
 
     "titleType" : "Type Visualization",
-    "titleTypeExplanation" : "Types with Number of Methods/Functions (NOM - y axis), Lines of Code (SLOC - x axis), Complexity (WMC - bubble color), and Dependencies (DEP - bubble size)",
+    "titleTypeExplanation" : "Types with Number of Methods/Functions (NOM - y-axis), Lines of Code (SLOC - x-axis), Complexity (WMC - bubble color), and Dependencies (DEP - bubble size)",
 
     "titleMethod" : "Method Visualization",
-    "titleMethodExplanation" : "Methods with Complexity (CYCLO - y axis), Lines of Code (MLOC - x axis), Number of Invocations (CALLS - bubble color), and Nested Block Depth (NBD - bubble size)",
+    "titleMethodExplanation" : "Methods with Complexity (CYCLO - y-axis), Lines of Code (MLOC - x-axis), Number of Invocations (CALLS - bubble color), and Nested Block Depth (NBD - bubble size)",
 
     "titleInternalDependencies" : "Internal Dependencies Visualization",
     "titleInternalDependenciesExplanation" : "Internal dependencies between type/classes",
@@ -71,12 +87,12 @@ var graphTitles = {
     "titleTypeCoupling1Explanation" : "Red lines (output coupling) and green lines (input coupling)",
 
     "titleTypeCoupling2" : "Type Coupling Visualization",
-    "titleTypeCoupling2Explanation" : "Types with I-DEP (y axis), DEP (x axis), FAN-IN (bubble color), and FAN-OUT (bubble size)",
+    "titleTypeCoupling2Explanation" : "Types with I-DEP (y-axis), DEP (x-axis), FAN-IN (bubble color), and FAN-OUT (bubble size)",
 
     "titleThresholds" : "Metric Thresholds Information",
 
     "titleInstability" : "Instability and Abstractness Visualization",
-    "titleInstabilityExplanation" : "Abstractness degree (y axis) and Instability (x axis)",
+    "titleInstabilityExplanation" : "Abstractness degree (y-axis) and Instability (x-axis)",
 
     "titleCyclicDependencies" : "Cyclic Dependencies Visualization",
     "titleCyclicDependenciesExplanation" : "Cyclic dependencies between type/classes"
